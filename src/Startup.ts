8377e2f8b3846940b86a9e275411249a86c9f6ea
Agustin <dimauloagustin@gmail.com>
2020-10-04 21:24:15 -0300

    Controller Test

--- src/Startup.ts
+++ src/Startup.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
+import StocksController from "./Controllers/StocksController";
 
 const startServer = () => {
   const app = express();
 
-  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
   //app.use(/*errorController.get404*/); //TODO - ADD error controller
+  app.use(new StocksController().router);
 
   app.listen(2999); //TODO - ADD to config file
 };
